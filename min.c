355813ef522dba872e358872cf658e154709624e
haoyu321 <115057471+haoyu321@users.noreply.github.com>
2022-10-05 11:50:24 +0800

    switch case

---
+++ min.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int grade;
+	scanf("%d",&grade);
+	switch(grade / 10)
+	{
+		case 10:
+		case 9:
+			printf("A");
+			break;
+		case 8:
+			printf("B");
+			break;
+		case 7:
+			printf("C");
+			break;
+		case 6:
+			printf("D");
+			break;
+		default:
+			printf("E");
+			break;
+	}
+	return 0;
+}
